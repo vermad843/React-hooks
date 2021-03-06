ecfa56fca91ea9ef6b7545139b6ebcd3dbf4e530
vermad843 <vishnuverma7524@gmail.com>
2021-03-07 01:26:09 +0530

    e.target.value

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
-import React, {useState} from 'react'
+import React, {useState,useCallback} from 'react'
 
 import './App.css';
 
 const App = () =>  {
   const [name, setName] = useState('CJ');
+
+const onNameChange = useCallback((e) => {
+  setName(e.target.value);
+}, []);
+
   return (
     <div className="App">
        <form>
             <label>Enter your name:</label>
-            <input onChange={(e) => {
-              console.log(e.target.value);
-            }}/>
+            <input 
+              value = {name}
+              onChange = {onNameChange}
+             />
        </form>
        <h1>Hello {name}</h1>
     </div>
