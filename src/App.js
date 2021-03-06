2e78e8ca92a0c9bd476bff444e796e979b67551b
vermad843 <vishnuverma7524@gmail.com>
2021-03-07 03:47:23 +0530

    allDone

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ const App = () =>  {
      if(!newTodo.trim()) return;
      setTodos([
        {
-         id : todos.length ? todos[todos.length - 1].id + 1 : 1,
+         id : todos.length ? todos[0].id + 1 : 1,
          content : newTodo,
          done : false,
        },
@@ -46,6 +46,17 @@ const App = () =>  {
     setTodos(todos.filter(otherTodo => otherTodo !== todo));
   }, [todos]);
 
+  
+  const markAllDone = useCallback(() => {
+       const updatedTodos = todos.map(todo => {
+         return {
+           ...todo,
+           done : true,
+         };
+       });
+      setTodos(updatedTodos);
+  }, [todos]);
+
   return (
     <div className="App">
        <form onSubmit = {formSubmitted}>
@@ -58,6 +69,7 @@ const App = () =>  {
              />
              <button type = "submit">Add Todo</button>
        </form>
+       <button onClick = {markAllDone}>Mark All Done</button>
           <ul>
              {todos.map((todo, index) => (
                <li key = {todo.id} >
